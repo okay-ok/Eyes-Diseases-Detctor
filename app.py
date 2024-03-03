53073421f824b8d82b50e6b18ddb41750cf277d9
Hiren <115985560+okay-ok@users.noreply.github.com>
2024-03-03 17:22:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,88 +51,21 @@ def main():
                 elif class2 > 3*class1:
                     st.markdown("Your model does not  by {:.2f}%".format(class2 * 100))
                 else:
-                    st.write("We encountered an ERROR. This should be temporary, please try again with a better quality image. Cheers!")
-            if dr:
-                model_d = tensorflow.keras.models.load_model('eye_models/dr/model.h5')
-                data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-                image = Image.open(image_input)
-                size = (224, 224)
-                image = ImageOps.fit(image, size,Image.LANCZOS)# Image.ANTIALIAS)
-                image_array = np.asarray(image)
-                normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-                data[0] = normalized_image_array
-                answer = model_d.predict(data)
-                class1 = answer[0,0]
-                class2 = answer[0,1]
-                if class1 > class2:
-                    st.write("Diabetic Retinopathy Detected. Confidence: {:.2f}".format(class1 * 100))
-                    st.write("-------------------------------")
-                elif class2 > class1:
-                    st.write("Diabetic Retinopathy Not Detected.")
-                    st.write("-------------------------------")
-            if r:
-                model_r = tensorflow.keras.models.load_model('eye_models/redness/model.h5')
-                data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-                image = Image.open(image_input)
-                size = (224, 224)
-                image = ImageOps.fit(image, size, Image.ANTIALIAS)
-                image_array = np.asarray(image)
-                image.show()
-                normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-                data[0] = normalized_image_array
-                answer = model_r.predict(data)
-                class1 = answer[0,0]
-                class2 = answer[0,1]
-                if class1 > class2:
-                    st.write("Redness Levels: {:.2f}%".format(class1 * 100))
-                    st.write("-------------------------------")
-                elif class2 > class1:
-                    st.write("No Redness Detected. Confidence: {:.2f}%".format(class2 * 100))
-                    st.write("-------------------------------")
+                    st.write("We encountered an ERROR in making a definite prediction. This should be temporary, please try again with a better quality image.")
+           
 
-    elif menu == "COVID":
-        st.sidebar.write("It uses CT Scans to detect whether the patient is likely to have COVID or not. Upload an image to get started.")
-        st.write("---------------------------")
-        st.set_option('deprecation.showfileUploaderEncoding', False)
-        image_input = st.sidebar.file_uploader("Choose a file: ", type=['png', 'jpg'])
-        if image_input:
-            img = image_input.getvalue()
-            analyze = st.sidebar.button("Analyze")
-            size = st.slider("Adjust image size: ", 300, 1000)
-            st.image(img, width=size, height=size)
-            st.write("-----------------------------------------")
-            np.set_printoptions(suppress=True)
-            model = tensorflow.keras.models.load_model('covid_model/model.h5')
-            data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-            if analyze: 
-                image = Image.open(image_input)
-                size = (224, 224)
-                image = ImageOps.fit(image, size, Image.ANTIALIAS)
-                image_array = np.asarray(image)
-                normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-                normalized_image_array.resize(data.shape)
-                data[0] = normalized_image_array
-                prediction = model.predict(data)
-                print(prediction)
-                class1 = prediction[0,0]
-                class2 = prediction[0,1]
-                if class2 > class1:
-                    st.markdown("**Possibility of COVID.** Confidence: {:.2f}%".format(class2 * 100))
-                elif class1 > class2:
-                    st.markdown("**Unlikely to have COVID** Confidence: {:.2f}".format(class1 * 100))
-                else:
-                    st.write("Error! Please upload a better quality image for accuracy.")
+    
                     
     elif menu == "Skin":
-        st.sidebar.write("It detects whether the patient has benign or malignant type of cancer. Further classifications are still under testing. Upload an image to get started.")
+        st.sidebar.write("Get Started.")
         st.write("---------------------------")
         st.set_option('deprecation.showfileUploaderEncoding', False)
         image_input = st.sidebar.file_uploader("Choose a file: ", type='jpg')
         if image_input:
             img = image_input.getvalue()
             analyze = st.sidebar.button("Analyze")
-            size = st.slider("Adjust image size: ", 300, 1000)
-            st.image(img, width=size, height=size)
+           
+            st.image(img)
             st.write("-----------------------------------------")
             np.set_printoptions(suppress=True)
             model = tensorflow.keras.models.load_model('skin_model/model.h5')
